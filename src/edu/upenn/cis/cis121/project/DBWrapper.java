386e8e07e4c781a3dd996a4150b4acf6f34c80bc
Tess Rinearson <tess.rinearson@gmail.com>
2012-04-27 03:33:37 -0400

    Finished DBWrapper methods for retrieving info from the database

--- src/edu/upenn/cis/cis121/project/DBWrapper.java
+++ src/edu/upenn/cis/cis121/project/DBWrapper.java
@@ -64,13 +64,55 @@ public class DBWrapper {
 	// returns the place_ids of all the places liked by the user with user_id
 	public int[] getLikes(int user_id)
 	{
+		ArrayList<Integer> likelist = new ArrayList<Integer>();
+		try {
+			String query = "select place_id from Likes where user_id=" + 
+					user_id;
+			
+			Statement st = _conn.createStatement();
+		    ResultSet rs = st.executeQuery(query);
+		    
+		    while (rs.next()) {
+		    	Integer lid = new Integer(rs.getInt(1));
+		    	likelist.add(lid);
+		    }
+		    
+		    rs.close();
+		    st.close();
+		} catch (SQLException sqle) {
+			System.err.println(sqle.toString());
+		}
+		
+		return convertToPrimitives(likelist);
 		
 	}
 	
 	
 	// returns an array of the form [lat,lon] representing the location
 	// of the place with place_id
-	//public double[] getLocation(int place_id);
+	public double[] getLocation(int place_id)
+	{
+		double[] loc = new double[2];
+		try {
+			String query = "select latitude, longitude from Places where " +
+					"place_id = " + place_id;
+			
+			Statement st = _conn.createStatement();
+		    ResultSet rs = st.executeQuery(query);
+		    
+		    while (rs.next()) {
+		    	loc[0] = rs.getInt("latitude");
+		    	loc[1] = rs.getInt("longitude");
+		    }
+		    
+		    rs.close();
+		    st.close();
+		} catch (SQLException sqle) {
+			System.err.println(sqle.toString());
+		}
+		
+		return loc;
+	}
 	
 	/**
 	 * Converts an ArrayList of Integers to an array of ints.
